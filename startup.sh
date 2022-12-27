dbb323b9cfcfe8e92d0d4f62a4fe201672784d15
Alex Ross <alexleroyross@gmail.com>
2022-12-27 12:58:24 -0500

    Add init.d commands

--- startup.sh
+++ startup.sh
@@ -1,1 +1,12 @@
-python3 main.py
+# /etc/init.d/startup.sh
+### BEGIN INIT INFO
+# Provides:          startup.sh
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start gmail pi app on boot
+# Description:       Start gmail pi app on boot
+### END INIT INFO
+
+python3 /home/pi/gmail-pi-applet/main.py
